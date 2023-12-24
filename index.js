bacfb1050853993b2a29d19cc166ad12d131b2a2
norristown <jarvasity@gmail.com>
2023-12-24 12:51:55 -0500

    exercises 3.13-3.14

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+require("dotenv").config();
 const express = require("express");
+
+const Phonebook = require("./models/phonebook");
 
 const app = express();
 const morgan = require("morgan");
@@ -35,25 +38,30 @@ app.get("/", (request, response) => {
 });
 
 app.get("/api/persons", (request, response) => {
-  response.json(phonebook);
+  Phonebook.find({}).then((person) => {
+    response.json(person);
+  });
 });
 
-app.get("/info", (request, response) => {
-  const time = new Date();
-  response.send(
-    `<p>Phonebook has info for ${phonebook.length} people</p><br/>
-    <p>${time}</p>`
-  );
-});
+// app.get("/info", (request, response) => {
+//   const time = new Date();
+//   response.send(
+//     `<p>Phonebook has info for ${Phonebook} people</p><br/>
+//     <p>${time}</p>`
+//   );
+// });
 
-const PORT = process.env.PORT || 3002;
+const { PORT } = process.env;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
 app.get("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const person = phonebook.find((person) => person.id === id);
+  const person = Phonebook.findById(request.params.id).then((item) => {
+    response.json(item);
+  });
+  // const id = Number(request.params.id);
+  // const person = phonebook.find((person) => person.id === id);
   if (person) {
     response.json(person);
   } else {
@@ -91,12 +99,21 @@ app.post("/api/persons", (request, response) => {
     });
   }
 
-  const person = {
-    id: Math.floor(Math.random() * 100 + 1),
+  const person = new Phonebook({
     name: body.name,
     number: body.number,
-  };
+  });
 
-  phonebook = phonebook.concat(person);
-  response.json(phonebook);
+  person.save().then((savedPerson) => {
+    response.json(savedPerson);
+  });
+
+  // const person = {
+  //   id: Math.floor(Math.random() * 100 + 1),
+  //   name: body.name,
+  //   number: body.number,
+  // };
+
+  // phonebook = phonebook.concat(person);
+  // response.json(phonebook);
 });
---
+++ models/phonebook.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+// const password = process.argv[2];
+// const name = process.argv[3];
+// const number = process.argv[4];
+
+const url = process.env.MONGODB_URI;
+console.log("connecting to", url);
+
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(url)
+  .then((result) => {
+    console.log("connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log("error connecting to MongoDB:", error.message);
+  });
+
+const phonebookSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+phonebookSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+module.exports = mongoose.model("Phonebook", phonebookSchema);
